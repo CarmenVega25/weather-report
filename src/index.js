c18a022e575fdaea89a2a7173d6ead800681fed9
Lynn <lynn@jansheski.com>
2022-12-07 14:59:16 -0800

    completed 3, 5 and 6. Waiting for access key for wave 4.

--- src/index.js
+++ src/index.js
@@ -19,7 +19,25 @@ const minusTemp = (event) => {
   state.totalCount -= 1;
   const tempCountCounter = document.querySelector('#totalCount');
   tempCountCounter.textContent = `Temp Count: ${state.totalCount}`;
+  updateTempColor(state.totalCount);
 };
+
+const resetCity = (event) => {
+  const reset = document.createElement('span');
+  const cityNameContainer = document.querySelector('#cityNameContainer');
+  cityNameContainer.appendChild(reset);
+  const resetButton = document.querySelector('#Reset');
+  userInput.textContent = `For the lovely city of Seattle`;
+};
+
+const input = document.querySelector('input');
+const userInput = document.getElementById('userInput');
+
+input.addEventListener('change', updateValue);
+
+function updateValue(e) {
+  userInput.textContent = `For the lovely city of ${e.target.value}`;
+}
 
 const registerEventHandlers = (event) => {
   console.log('in registerEventHandlers:', event);
@@ -27,6 +45,9 @@ const registerEventHandlers = (event) => {
   const minusTempButton = document.querySelector('#decreaseTemp');
   addTempButton.addEventListener('click', addTemp);
   minusTempButton.addEventListener('click', minusTemp);
+  const userInput = document.querySelector('#userInput');
+  const resetButton = document.querySelector('#Reset');
+  resetButton.addEventListener('click', resetCity);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
@@ -54,3 +75,47 @@ const updateTempColor = (totalCount) => {
       '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
   }
 };
+
+const selectElement = document.querySelector('.skyDropdown');
+
+selectElement.addEventListener('change', (event) => {
+  const result = document.querySelector('.result');
+  if (event.target.value === 'snowy') {
+    result.textContent = `🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨`;
+  } else if (event.target.value === 'rainy') {
+    result.textContent = `🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧`;
+  } else if (event.target.value === 'cloudy') {
+    result.textContent = `☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️`;
+  } else if (event.target.value === 'sunny') {
+    result.textContent = `☁️ ☁️ ☁️ ☀️ ☁️ ☁️`;
+  }
+});
+
+// add api key
+
+const getCityLoc = () => {
+  axios
+    .get(API, {
+      params: {
+        key: apiKey,
+        q: 'Seattle',
+        limit: 1,
+        format: 'json',
+      },
+      headers: {
+        'Accept-Encoding': 'application/json',
+      },
+    })
+    .then((result) => {
+      // console.log(result);
+      const lat = result.data[0].lat;
+      const lon = result.data[0].lon;
+      console.log(`Seattle lat: ${lat} lon: ${lon}`);
+    })
+
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+console.log(getCityLoc());
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,26 @@
+const getCityLoc = () => {
+//   axios
+//     .get(API, {
+//       params: {
+//         key: key,
+//         q: 'Seattle',
+//         limit: 1,
+//         format: 'json',
+//       },
+//       headers: {
+//         'Accept-Encoding': 'application/json',
+//       },
+//     })
+//     .then((result) => {
+//       // console.log(result);
+//       const lat = result.data[0].lat;
+//       const lon = result.data[0].lon;
+//       console.log(`Seattle lat: ${lat} lon: ${lon}`);
+//     })
+
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
+
+// console.log(getCityLoc());
